7ac4c9a54502682f54644c76ed749bbb1beb5d22
MCreese2 <murraycreese@gmail.com>
2019-09-24 15:09:48 +0000

    Multiline print/home/gitpod/.pyenv/versions/3.7.4/bin/python3

---
+++ 1-basics/1-output/2-multiline-message/bot.py
@@ -0,0 +1,9 @@
+#Display message to the standard output
+print("System Failure Imminenet!")
+print("System Reboot had been Initiated...")
+print()
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
+print()
+print("System Online.")
