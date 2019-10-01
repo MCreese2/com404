874d8ade4bef56c72837df7ee068041091849764
MCreese2 <murraycreese@gmail.com>
2019-10-01 14:07:38 +0000

    step 6 done

--- 1-basics/3-Decision/4-modulo-operator/bot.py
+++ 1-basics/3-Decision/4-modulo-operator/bot.py
@@ -1,4 +1,4 @@
-
+# % For example, 10 % 3 divides 10 by 3 and returns a remainder of 1.
 whole_number = int(input("Please enter a whole number "))
 if (whole_number % 2):
     print("The number ", whole_number, " is an odd number.")
