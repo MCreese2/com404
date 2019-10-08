b328b5c376fd42add9702dd30f03a5ee76dc49f6
MCreese2 <murraycreese@gmail.com>
2019-10-08 13:22:21 +0000

    Cables

--- 1-basics/4-repetition/1-while-loop/1-simple/bot.py
+++ 1-basics/4-repetition/1-while-loop/1-simple/bot.py
@@ -1,15 +1,15 @@
-#Declare the variable for removed cables
-
 #Asks the user to input how many cables to remove
 removed_cables = int(input("How many cables should I remove? "))
 
 #Displays the users unput
-print(removed_cables, " cables!")
+print(removed_cables, "cables!")
 
 #Declares the variable for counting the cables
 cable_count = 0
 
+#Counts cables until the user's number is met
 while cable_count < removed_cables:
     cable_count = cable_count + 1
     print("Removed cable.")
 
+print("Done!")
